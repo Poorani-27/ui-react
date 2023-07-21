74cc0bd970298acef6a22583235b7ac56157dee7
Poorani <tpoorani2002@gmail.com>
2023-07-21 23:53:55 +0530

    build process

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const [toggleMenu, setToggleMenu] = useState (false);
     </div>
     <ul className='app__navbar-links'>
       <li className='p__opensans'><a href="#home">Home</a></li>
-      <li className='p__opensans'><a href="#about">About</a></li>
+      <li className='p__opensans'><a href="#about">About Us</a></li>
       <li className='p__opensans'><a href="#menu">Menu</a></li>
       
       <li className='p__opensans'><a href="contact">Contact</a></li>
